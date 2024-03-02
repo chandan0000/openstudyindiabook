ac244f7c66d0bdc2f31484983504f1cf784271f1
chandan0000 <kumarchandan41u@gmail.com>
2024-03-02 10:28:20 +0530

    .

---
+++ src/routes/books.rs
@@ -0,0 +1,84 @@
+use axum::{
+    extract::{Multipart, State},
+    http::StatusCode,
+};
+use chrono::{DateTime, Utc};
+use futures::{future::ok, io, StreamExt, TryStreamExt};
+
+use sea_orm::DatabaseConnection;
+
+use tokio::{fs::File, io::AsyncWriteExt};
+use tokio_util::io::StreamReader;
+
+pub async fn book_created(
+    State(databse): State<DatabaseConnection>,
+    mut multipart: Multipart,
+) -> Result<(), StatusCode> {
+    while let Some(field) = multipart.next_field().await.unwrap() {
+        let field_name = field.name().unwrap().to_string();
+        let timenow = Utc::now().timestamp();
+        if field_name == "image" {
+            let data = field.bytes().await.unwrap();
+            let mut file = File::create(format!("./public/uploads/{}.png", timenow,))
+                .await
+                .unwrap();
+
+            file.write(&data).await.unwrap();
+        } else {
+            let data = field.text().await.unwrap();
+            println!("field: {}      value: {}", field_name, data);
+        }
+    }
+
+    // while let Some(field) = multipart.next_field().await.unwrap() {
+    //     let filename = if let Some(filename) = field.file_name() {
+    //         filename.to_string()
+    //     } else {
+
+    //         continue;
+    //     };
+
+    //     // let options = UploadOptions::new().set_public_id(filename.to_string());
+    //     // let upload = Upload::new(
+    //     //     "153129147683852".to_string(),
+    //     //     "dv4uxyxoj".to_string(),
+    //     //     "yiKijdDX4c0YqRkw5wscSnxmpDw".to_string(),
+    //     // );
+
+    //     // futures::pin_mut!(body_reader);
+    // }
+
+    Ok(())
+}
+pub async fn upload(mut multipart: Multipart) {
+    while let Some(field) = multipart.next_field().await.unwrap() {
+        let filename = if let Some(filename) = field.file_name() {
+            filename.to_string()
+        } else {
+            continue;
+        };
+
+        // let bucket = Bucket::new(
+        //     "test",
+        //     "us-east-1".parse().unwrap(),
+        //     Credentials::default().unwrap(),
+        // )
+        // .unwrap();
+
+        let body_with_io_error = field.map_err(|err| io::Error::new(io::ErrorKind::Other, err));
+
+        let body_reader = StreamReader::new(body_with_io_error);
+
+        futures::pin_mut!(body_reader);
+
+        // // put_file(bucket, &filename, body_reader).await.unwrap();
+
+        // return Ok(Response::builder()
+        //     .status(StatusCode::CREATED)
+        //     .body(boxed("OK".to_string()))
+        //     .unwrap());
+        todo!()
+    }
+
+    // Err(StatusCode::INTERNAL_SERVER_ERROR)
+}
--- src/routes/mod.rs
+++ src/routes/mod.rs
@@ -1,7 +1,7 @@
+mod books;
 mod category;
 mod hello_server;
 mod users;
-
 use category::create_category;
 use hello_server::hello_server;
 use users::{create_user, login_user, logout_user};
@@ -10,8 +10,10 @@ use axum::{
     extract::FromRef,
     middleware,
     routing::{get, post},
+    routing::{on, MethodFilter},
     Router,
 };
+use books::book_created;
 use sea_orm::DatabaseConnection;
 
 use crate::utils::guards::guard;
@@ -25,12 +27,15 @@ pub struct AppState {
 pub fn create_routes(database: DatabaseConnection) -> Router {
     let app_state = AppState { database };
     Router::new()
-      
         .route("/logout", post(logout_user))
         .route_layer(middleware::from_fn_with_state(app_state.clone(), guard))
         //
         .route("/", get(hello_server))
         .route("/category", post(create_category))
+        .route(
+            "/books",
+            post(book_created), // on(MethodFilter::POST, book_created)
+        )
         .route("/users/signup", post(create_user))
         .route("/users/login", post(login_user))
         .with_state(app_state)
